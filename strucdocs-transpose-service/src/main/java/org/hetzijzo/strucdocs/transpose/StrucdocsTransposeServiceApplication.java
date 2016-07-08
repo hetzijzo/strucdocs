91611aefe27e243c724998298cfa333219896c8a
Willem Cheizoo <willem.cheizoo@philips.com>
2016-07-08 15:06:36 +0200

    Add Transpose Service

---
+++ strucdocs-transpose-service/src/main/java/org/hetzijzo/strucdocs/transpose/StrucdocsTransposeServiceApplication.java
@@ -0,0 +1,12 @@
+package org.hetzijzo.strucdocs.transpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrucdocsTransposeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrucdocsTransposeServiceApplication.class, args);
+	}
+}

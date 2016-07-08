0ed8347841ef119ebfbb1da766c65534860415dd
Willem Cheizoo <willem.cheizoo@philips.com>
2016-07-08 14:53:52 +0200

    Add edge service

---
+++ strucdocs-edge/src/main/java/org/hetzijzo/strucdocs/StrucdocsEdgeApplication.java
@@ -0,0 +1,12 @@
+package org.hetzijzo.strucdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrucdocsEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrucdocsEdgeApplication.class, args);
+	}
+}
---
+++ strucdocs-edge/src/test/java/org/hetzijzo/strucdocs/StrucdocsEdgeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.hetzijzo.strucdocs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = StrucdocsEdgeApplication.class)
+public class StrucdocsEdgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

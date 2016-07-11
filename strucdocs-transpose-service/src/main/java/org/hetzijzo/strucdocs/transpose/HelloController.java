0d912f4f284ed026ffb926f83c7d30aad278530f
Willem Cheizoo <w.cheizoo@gmail.com>
2016-07-11 15:57:04 +0200

    Add more functionality to all services. Register them to eureka.

--- strucdocs-transpose-service/src/main/java/org/hetzijzo/strucdocs/transpose/HelloController.java
+++ strucdocs-transpose-service/src/main/java/org/hetzijzo/strucdocs/transpose/HelloController.java
@@ -8,7 +8,7 @@
  * of ROYAL PHILIPS N.V.
  */
 
-package org.hetzijzo.strucdocs.eureka;
+package org.hetzijzo.strucdocs.transpose;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;

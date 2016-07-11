8ff59212bf5f8d1d5b3387ec7c4d0f941fbf0fb6
Willem Cheizoo <w.cheizoo@gmail.com>
2016-07-11 10:53:23 +0200

    Add zipkin. And set jcenter instead of mavenCentral.

---
+++ strucdocs-zipkin/src/main/java/org/hetzijzo/strucdocs/zipkin/StrucdocsZipkinApplication.java
@@ -0,0 +1,12 @@
+package org.hetzijzo.strucdocs.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrucdocsZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrucdocsZipkinApplication.class, args);
+    }
+}

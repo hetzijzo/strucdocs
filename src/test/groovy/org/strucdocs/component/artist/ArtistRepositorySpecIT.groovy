49fa7d633cff790f5d28794ba8904735099b7807
Willem Cheizoo <willem.cheizoo@jdriven.com>
2017-01-02 22:39:32 +0100

    Improve test for artist

--- src/test/groovy/org/strucdocs/component/artist/ArtistRepositorySpecIT.groovy
+++ src/test/groovy/org/strucdocs/component/artist/ArtistRepositorySpecIT.groovy
@@ -13,8 +13,7 @@ import org.strucdocs.model.Artist
 
 import javax.transaction.Transactional
 
-import static org.springframework.http.HttpMethod.GET
-import static org.springframework.http.HttpMethod.POST
+import static org.springframework.http.HttpMethod.*
 import static org.springframework.http.HttpStatus.CREATED
 import static org.springframework.http.HttpStatus.OK
 
@@ -33,10 +32,10 @@ class ArtistRepositorySpecIT extends RestOAuthAbstractSpecification {
         then: "A status 200 should be returned"
             response.statusCode == OK
         then: "An empty list of arrays should be returned"
-            response.getBody().getContent().size() == 0
+            response.body.content.size() == 0
     }
 
-    def "should save and return artist"() {
+    def "should save, update and delete artist"() {
         given: "An valid artists"
             Artist artist = Artist.builder().name("Tina Turner").build()
         when: "This artist is save"
@@ -50,12 +49,32 @@ class ArtistRepositorySpecIT extends RestOAuthAbstractSpecification {
             ResponseEntity<Resources<Resource<Artist>>> responseList = restTemplate.exchange(
                 '/artists', GET, new HttpEntity(headers),
                 new ParameterizedTypeReference<Resources<Resource<Artist>>>() {})
-        then: "The new artists should be returned"
+        then: "The new artist should be included"
             Collection<Resource<Artist>> artists = responseList.getBody().getContent()
             artists.size() == 1
-            with(artists.content.iterator().next()) {
+
+        when: "The new artist link is used"
+            Resource<Artist> artistResource = artists.iterator().next()
+            artist = artistResource.content
+
+            ResponseEntity<Resource<Artist>> artistResponse = restTemplate.exchange(
+                artistResource.getLink("self").href, GET, new HttpEntity(headers),
+                new ParameterizedTypeReference<Resource<Artist>>() {})
+
+        then: "The new artist should have the correct content"
+            with(artistResponse.body.content) {
                 [name] == ['Tina Turner']
                 [uuid] != null
+            }
+
+        when: "The artist is updated"
+            artist.name = 'Tina Turner2'
+            artistResponse = restTemplate.exchange(
+                artistResource.getLink("self").href, PUT, new HttpEntity(artist, headers),
+                new ParameterizedTypeReference<Resource<Artist>>() {})
+        then: "The updated artist should be returned"
+            with(artistResponse.body.content) {
+                [name] == ['Tina Turner2']
             }
     }
 }
